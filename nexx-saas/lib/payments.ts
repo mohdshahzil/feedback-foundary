8afd82359d381d1d5c303b7e34630bf7b10f8ddd
mohdshahzil <theshahzil@gmail.com>
2024-11-05 22:18:57 +0530

    initial commit

---
+++ nexx-saas/lib/payments.ts
@@ -0,0 +1,3 @@
+export const monthlyPlanId: string = "price_1QFKrNFl8WYta1I7lxV8jvDC";
+export const yearlyPlanId: string = "price_1QFKs0Fl8WYta1I7bkYsn4me";
+export const maxFreeProjects: number = 3;
